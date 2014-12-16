44c2a56c6b22c7fe8e25f6f0c17f952f19ff29f4
Lewis Fishgold <lewfish@gmail.com>
2014-12-16 16:49:20 -0500

    Add ability to log in, log out, sign up, and make login required for submit and vote

--- social_news/urls.py
+++ social_news/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 from . import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.all, name='all'),
-                       url(r'^submit/$', views.EntryCreateView.as_view(), name='submit'),
-                       url(r'^vote/$', views.vote, name='vote'),
+                       url(r'^submit/$', login_required(views.EntryCreateView.as_view()), name='submit'),
+                       url(r'^vote/$', login_required(views.vote), name='vote'),
+                       url(r'^login/$', 'django.contrib.auth.views.login', 
+                           {'template_name': 'social_news/login.html'},
+                           name='login'),
+                       url(r'^logout/$', 'django.contrib.auth.views.logout', 
+                           {'template_name': 'social_news/logout.html'},
+                           name='logout'),
+                       url(r'^signup/$', CreateView.as_view(template_name='social_news/signup.html', form_class=UserCreationForm, success_url='/'), name = 'signup'),
 )
